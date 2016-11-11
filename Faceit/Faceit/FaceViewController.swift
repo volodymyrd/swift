79504c6159402428588d7cd84d964087ec688956
volodymyrd <volodymyrdotsenko@gmail.com>
2016-11-11 17:52:23 +0100

    Faceii

---
+++ Faceit/Faceit/FaceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Volodymyr Dotsenko on 11/9/16.
+//  Copyright © 2016 Volodymyr Dotsenko. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+}
